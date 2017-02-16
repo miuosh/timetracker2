cfdeff86592dce24ef661b9e5d5f6e78a9cbb18d
miuosh <jobo99@wp.pl>
2017-02-16 13:05:33 +0100

    Revert "Revert "add myTimePicker directive""
    This reverts commit 98c69d24ee4786e0228a2769456ef1615324a94a.

--- public/layout.home/dayview.controller.js
+++ public/layout.home/dayview.controller.js
@@ -25,15 +25,18 @@
     vm.nextDay = nextDay;
 
     // filter table
-    $scope.sortType = 'timespan.startTime';
+    $scope.sortType = '';
     $scope.sortReverse = true;
     $scope.searchType = '';
 
-    //edit task dialog
-    vm.openEditDialog = openEditDialog;
-
     // edit model
     vm.editTaskHistoryItem = editTaskHistoryItem;
+    $scope.edit = false;
+
+    /* timespan edition  */
+    vm.cancelEdit = cancelEdit;
+    vm.saveChanges = saveChanges;
+    vm.editMode = editMode;
 
     /* internal functions */
     var sumByProperty = sumByProperty;
@@ -51,37 +54,7 @@
 
   }
 
-  function openEditDialog(ev, item) {
-    console.log('OpenEditDialog: item: \n');
-    console.log(item);
-    return dataservice.getTask(item._id)
-      .then(function(data) {
-        console.log('Received data: ');
-        console.log(data);
 
-        $mdDialog.show({
-          locals: {
-            task: data[0]
-          },
-          controller: 'DialogTaskEditController',
-          controllerAs: 'edc',
-          templateUrl: '/layout.home/dialog.task.edit.html',
-          parent: angular.element(document.body),
-          targetEvent: ev,
-          clickOutsideToClose: true,
-          fullscreen: true
-        })
-        .then(function(answer) {
-          console.log('Dialog OK: ' + answer);
-          getTasksByDate(vm.viewDate);
-        }, function(){
-          console.log('Cancel dialog.');
-        });
-      })
-      .catch(function(err) {
-        console.log(err);
-      });
-    }
 
     function getTasksByDate(date) {
       return dataservice.getTasksByDate(date.getTime())
@@ -153,12 +126,25 @@
     }
 
     function sumByProperty(items, property) {
-      console.log()
       if (items === 0) return 0;
-
       return items.reduce(function(previous, current) {
         return current[property] === null ? previous : previous + parseFloat(current[property]);
       }, 0)
+    }
+
+
+    /* taks edition */
+
+    function cancelEdit() {
+      init();
+    }
+
+    function editMode() {
+
+    }
+
+    function saveChanges() {
+      init();
     }
 
   }// #DayViewController
